140e6a747dcd0ecc2e6aad4860ad4f83037c9cd3
Tamerlan <tamerlan-aliyev-2021@mail.ru>
2022-07-23 17:59:18 +0400

    finish heywanna finaly

--- Heywanna/animal/charity/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Charity(models.Model):
-    username = models.CharField(max_length=200)
-    password = models.CharField(max_length=200)
-    money = models.IntegerField()
-    def __str__(self):
-        return self.username
--- Heywanna/animal/charity/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path,include
-from charity import views
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',views.home,name='home'),
-]
